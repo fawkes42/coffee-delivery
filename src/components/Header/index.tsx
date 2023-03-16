5e4b656251404ac5965e25e4f01a97f6a00390a5
Pedro Tozzi <pedrogbtozzi@gmail.com>
2023-03-16 00:49:36 -0300

    Corrigindo cores logo no dark theme

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,16 @@
 import { HeaderContainer } from "./styles";
 import Logo from "../../assets/logo.svg";
+import LogoDark from "../../assets/logo-dark.svg";
 import { ShoppingCart, MapPin } from "phosphor-react";
 import { NavLink } from "react-router-dom";
+import { useContext } from "react";
+import { ThemeContext } from "styled-components";
+
 export function Header() {
+    const themeContext = useContext(ThemeContext)
     return (
         <HeaderContainer>
-            <img src={Logo} alt="Logo" />
+            <img src={themeContext.variant === 'default' ? Logo : LogoDark} alt="Logo" />
             <nav>
                 <NavLink to="/" title="Locale">
                     <MapPin size={24} weight="fill" />
